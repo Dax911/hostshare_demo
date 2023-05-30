84efa3d5bbc6cda366247fda2f20f70e8b8fb627
xponentialai <haydenaylor911@gmail.com>
2023-05-30 16:55:15 -0500

    :beers: working

--- src/components/searchbox.tsx
+++ src/components/searchbox.tsx
@@ -1,14 +1,27 @@
 import algoliasearch from 'algoliasearch/lite';
-import { Hits, InstantSearch, SearchBox } from 'react-instantsearch-hooks-web';
+import { InstantSearch, SearchBox, connectHits } from 'react-instantsearch-dom';
 import { ResultsCards } from './cards';
 
 const searchClient = algoliasearch('87MROQTWGM', '5da1787cc154b7bbde57ffd5acf84808');
+
+const Hits = ({ hits }) => (
+  <div className="space-y-4">
+    {hits.map(hit => (
+      <div key={hit.objectID} className="p-4 border rounded-md">
+        {/* Customize this div as needed */}
+        <p className="text-lg font-semibold">{hit.info.title}</p>
+      </div>
+    ))}
+  </div>
+);
+
+const CustomHits = connectHits(Hits);
 
 function SearchBarComp() {
   return (
     <InstantSearch searchClient={searchClient} indexName="dev_hostdemo2">
       <SearchBox />
-      <Hits />
+      <CustomHits/>
     </InstantSearch>
   );
 }
