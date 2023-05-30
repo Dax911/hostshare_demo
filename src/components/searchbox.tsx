2fb0c7b4aef2119fb72ca7046a85b4f1ed48dc4d
xponentialai <haydenaylor911@gmail.com>
2023-05-30 15:54:33 -0500

    :ambulance: type broke
    not sure what the types r

--- src/components/searchbox.tsx
+++ src/components/searchbox.tsx
@@ -3,16 +3,7 @@ import { Hits, InstantSearch, SearchBox } from 'react-instantsearch-hooks-web';
 import { ResultsCards } from './cards';
 
 const searchClient = algoliasearch('87MROQTWGM', '5da1787cc154b7bbde57ffd5acf84808');
-export function Hit({ hit }) {
-  return (
-    <article>
-      <img src={hit.image} alt={hit.name} />
-      <p>{hit.categories}</p>
-      <h1>{hit.name}</h1>
-      <p>${hit.price}</p>
-    </article>
-  );
-}
+
 function SearchBarComp() {
   return (
     <InstantSearch searchClient={searchClient} indexName="dev_hostdemo2">
