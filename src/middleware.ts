b3a7d27e6db5709f47999ea5fa5ab85e15894a5f
xponentialai <haydenaylor911@gmail.com>
2023-05-31 11:21:13 -0500

    :fire: cleaning house
    clearing dead code from other project

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,17 +4,10 @@ import { NextResponse, userAgent } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 const publicPaths = ['/', '/sign-in*', '/sign-up*', '/api/trpc/example*']
-const featuresPaths = ['/features', '/docs', '/blog']
 
 
 const isPublic = (path: string) => {
   return publicPaths.find((x) =>
-    path.match(new RegExp(`^${x}$`.replace('*$', '($|/|\\.)'))),
-  )
-}
-
-const isFeatures = (path: string) => {
-  return featuresPaths.find((x) =>
     path.match(new RegExp(`^${x}$`.replace('*$', '($|/|\\.)'))),
   )
 }
