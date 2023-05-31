e7d8e392faec6f5f51ee9e61ab15b40344ff0f10
xponentialai <haydenaylor911@gmail.com>
2023-05-30 21:44:28 -0500

    :clown_face: beginning cleanup
    insanity cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,29 +6,30 @@ import { AlertTriangle } from "lucide-react";
 
 import { Alert, AlertDescription, AlertTitle } from "../components/ui/alert";
 
-import {
-  InstantSearch,
-  Hits,
-  Highlight,
-  Snippet,
-  SearchBox,
-} from "react-instantsearch-hooks-web";
+
 import SearchBarComp from "../components/searchbox";
 
 function AlertDemo() {
   return (
+    <div className="max-w-[700px] items-center pb-4">
+
     <Alert variant={"destructive"}>
       <AlertTriangle className="h-4 w-4" />
       <AlertTitle>Heads up!</AlertTitle>
-      <AlertDescription>This is a Demo.</AlertDescription>
+      <AlertDescription>While this is a demo it does use an AI recommendation engine. This both supplies the demo with customizable recommendations and keeps me from being eaten alive by API costs. So please login or signup.</AlertDescription>
     </Alert>
+    </div>
+
   );
 }
 
 export default function IndexPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="my-20 flex flex-col items-center gap-2">
+
+      <div className="flex flex-col items-center gap-2">
+      <AlertDemo />
+
         <h1 className="font-Recoleta text-3xl font-bold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
           Hosts travel for free. <br className="hidden sm:inline" />
         </h1>
@@ -36,6 +37,7 @@ export default function IndexPage() {
           Share your unbooked nights with other hosts, and experience a new way
           to travel - free.
         </p>
+
         <div className="flex items-center gap-4">
           <Link
             href="/user"
@@ -47,10 +49,10 @@ export default function IndexPage() {
           <Link
             target="_blank"
             rel="noreferrer"
-            href="/browse"
+            href="/search"
             className={buttonVariants({ variant: "outline", size: "lg" })}
           >
-            Browse
+            Search
           </Link>
           </div>
           <SearchBarComp />
